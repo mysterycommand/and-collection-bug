4eecfe81bea9b26b0a03bf4318dda20ad67b3f66
Matt Hayes <matt@mysterycommand.com>
2014-08-27 15:21:10 -0700

    Get a test to run. Add some more config.

--- test/index.js
+++ test/index.js
@@ -1,3 +1,9 @@
 'use strict';
 
-module.exports = function() {};
+var expect = require('chai').expect;
+
+describe('Tests', function() {
+    it('should run', function() {
+        expect(true).to.be.true;
+    });
+});
